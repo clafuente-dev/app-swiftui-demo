d0bfb3f654f6bd37f20dfafb8b447a8f789540e2
Constanza Lafuente <clafuente@smu.cl>
2020-12-15 20:11:00 -0300

    [AU-12] Adds base project

---
+++ app-unimarc-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app-unimarc-ios
+//
+//  Created by Constanza on 15-12-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app-unimarc-ios/app_unimarc_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_unimarc_iosApp.swift
+//  app-unimarc-ios
+//
+//  Created by Constanza on 15-12-20.
+//
+
+import SwiftUI
+
+@main
+struct app_unimarc_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
